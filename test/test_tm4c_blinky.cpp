a8e762519ab15f1e3761638e9dd8e21c680849e7
Esteban Duran <astroesteban@icloud.com>
2022-01-21 12:42:11 -0600

    Make Test Modern C++

--- test/test_tm4c_blinky.cpp
+++ test/test_tm4c_blinky.cpp
@@ -9,26 +9,35 @@
  */
 #include <cstdint>
 
-#define GPIO_PORTF_DATA_R (*((volatile unsigned long *)0x400253FC))
-#define GPIO_PORTF_DIR_R (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_AFSEL_R (*((volatile unsigned long *)0x40025420))
-#define GPIO_PORTF_PUR_R (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_LOCK_R (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_AMSEL_R (*((volatile unsigned long *)0x40025528))
-#define GPIO_PORTF_PCTL_R (*((volatile unsigned long *)0x4002552C))
-#define SYSCTL_RCGC2_R (*((volatile unsigned long *)0x400FE108))
+// Wrap everything in an anonymous namespace so that the compiler optimizes
+// this away
+namespace {
+    volatile uint32_t &memory(const uint32_t loc) {
+        return *reinterpret_cast<uint32_t*>(loc);
+    }
 
-// The onboard LED colors
-#define LED_OFF 0
-#define LED_RED 0x02
-#define LED_BLUE 0x04
-#define LED_PINK 0x06
-#define LED_GREEN 0x08
-#define LED_YELLOW 0x0A
-#define LED_SKY_BLUE 0x0C
-#define LED_WHITE 0x0E
+    // Registers
+    static constexpr uint32_t GPIO_PORTF_DATA_R = 0x400253FC;
+    static constexpr uint32_t GPIO_PORTF_DIR_R = 0x40025400;
+    static constexpr uint32_t GPIO_PORTF_AFSEL_R = 0x40025420;
+    static constexpr uint32_t GPIO_PORTF_PUR_R = 0x40025510;
+    static constexpr uint32_t GPIO_PORTF_DEN_R = 0x4002551C;
+    static constexpr uint32_t GPIO_PORTF_LOCK_R = 0x40025520;
+    static constexpr uint32_t GPIO_PORTF_CR_R = 0x40025524;
+    static constexpr uint32_t GPIO_PORTF_AMSEL_R = 0x40025528;
+    static constexpr uint32_t GPIO_PORTF_PCTL_R = 0x4002552C;
+    static constexpr uint32_t SYSCTL_RCGC2_R = 0x400FE108;
+
+    // The onboard LED colors
+    static constexpr uint32_t LED_OFF = 0;
+    static constexpr uint32_t LED_RED = 0x02;
+    static constexpr uint32_t LED_BLUE = 0x04;
+    static constexpr uint32_t LED_PINK = 0x06;
+    static constexpr uint32_t LED_GREEN = 0x08;
+    static constexpr uint32_t LED_YELLOW = 0x0A;
+    static constexpr uint32_t LED_SKY_BLUE = 0x0C;
+    static constexpr uint32_t LED_WHITE = 0x0E;
+}
 
 /**
  * @brief Initializes Port F
@@ -36,16 +45,16 @@
  */
 void portFInit() {
     volatile uint32_t delay; // Give clock enough time to stabilize
-    SYSCTL_RCGC2_R |= 0x00000020; // 1) F clock
+    memory(SYSCTL_RCGC2_R) |= 0x00000020; // 1) F clock
     delay = SYSCTL_RCGC2_R; // delay
-    GPIO_PORTF_LOCK_R = 0x4C4F434B; // 2) unlock PortF PF0
-    GPIO_PORTF_CR_R = 0x1F; // allow changes to PF4-0
-    GPIO_PORTF_AMSEL_R = 0x00; // 3) disable analog function
-    GPIO_PORTF_PCTL_R = 0x00000000; // 4) GPIO clear bit PCTL
-    GPIO_PORTF_DIR_R = 0x0E; // 5) PF4,PF0 input, PF3,PF2,PF1 output
-    GPIO_PORTF_AFSEL_R = 0x00; // 6) no alternate function
-    GPIO_PORTF_PUR_R = 0x11; // enable pullup resistors on PF4,PF0
-    GPIO_PORTF_DEN_R = 0x1F; // 7) enable digital pins PF4-PF0
+    memory(GPIO_PORTF_LOCK_R) = 0x4C4F434B; // 2) unlock PortF PF0
+    memory(GPIO_PORTF_CR_R) = 0x1F; // allow changes to PF4-0
+    memory(GPIO_PORTF_AMSEL_R) = 0x00; // 3) disable analog function
+    memory(GPIO_PORTF_PCTL_R) = 0x00000000; // 4) GPIO clear bit PCTL
+    memory(GPIO_PORTF_DIR_R) = 0x0E; // 5) PF4,PF0 input, PF3,PF2,PF1 output
+    memory(GPIO_PORTF_AFSEL_R) = 0x00; // 6) no alternate function
+    memory(GPIO_PORTF_PUR_R) = 0x11; // enable pullup resistors on PF4,PF0
+    memory(GPIO_PORTF_DEN_R) = 0x1F; // 7) enable digital pins PF4-PF0
 }
 
 
@@ -73,11 +82,11 @@ int main() {
     portFInit();
 
     while (true) {
-        GPIO_PORTF_DATA_R = LED_RED;
+        memory(GPIO_PORTF_DATA_R) = LED_RED;
         delay();
-        GPIO_PORTF_DATA_R = LED_WHITE;
+        memory(GPIO_PORTF_DATA_R) = LED_WHITE;
         delay();
-        GPIO_PORTF_DATA_R = LED_BLUE;
+        memory(GPIO_PORTF_DATA_R) = LED_BLUE;
         delay();
     }
 }
